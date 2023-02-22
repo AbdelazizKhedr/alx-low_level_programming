4712264f2806d2205dae224b5b8121adaa46cde8
Abdelaziz Khedr <Azkh@gmail.com>
2023-02-22 04:07:15 +0200

    Hotfix: Solved a variable error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,12 +10,10 @@ void print_times_table(int n)
 {
 	if (n < 0 || n > 15)
 		return;
-	int i;
+	int i, j;
 
 	for (i = 0; i <= n; i++)
 	{
-		int j;
-
 		for (j = 0; j <= n; j++)
 		{
 			int z = i * j;
