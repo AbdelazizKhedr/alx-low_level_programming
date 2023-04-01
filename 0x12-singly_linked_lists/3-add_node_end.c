080ec1dfff34e6b4e5c68c679a289d7a507a229f
Abdelaziz Khedr <Azkh@gmail.com>
2023-04-01 02:22:35 +0200

    Hotfix:error 12

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,69 +1,44 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
-/**
- * _strlen - gets length of the string
- * @s: string
- * Return: length of the string
- */
-int _strlen(const char *s)
-{
-	int i;
+#include <string.h>
 
-	for (i = 0; s[i]; i++)
-		;
-	return (i);
-}
 /**
- * _strdup - recreation of string duplicate function
- * @src: source of string to duplicate
- * Return: pointer to malloc'd space with copied string
- */
-void *_strdup(const char *src)
-{
-	int len, i;
-	char *dest;
-
-	len = _strlen(src);
-	dest = malloc((len + 1) * sizeof(char));
-	if (dest == NULL)
-		return (NULL);
-	for (i = 0; src[i]; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
-}
-/**
- * add_node_end - add new nodes to the end of the list
- * @head: current place in the list
- * @str: string to add to the head
- * Return: pointer to current position in list
+ * add_node_end - add a new node at the end of the linked list
+ * @head: pointer to the the first list node pointer
+ * @str: the string value of the node
+ *
+ * Return: the pointer to the created node
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new, *current;
-	char *dupstr;
+	list_t *node;
 
-	if (str == NULL)
+	if (!head)
 		return (NULL);
-	dupstr = _strdup(str);
-	if (dupstr == NULL)
+	node = malloc(sizeof(list_t));
+	if (!node)
 		return (NULL);
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = dupstr;
-	new->len = _strlen(str);
-	new->next = NULL;
-	if (*head == NULL)
+
+	node->str = strdup(str);
+	if (!node->str)
 	{
-		*head = new;
-		return (*head);
+		free(node);
+		return (NULL);
 	}
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
-	current->next = new;
-	return (*head);
+
+	node->len = strlen(str);
+	if (!*head)
+	{
+		*head = node;
+	}
+	else
+	{
+		list_t *h = *head;
+
+		while (h->next)
+			h = h->next;
+		h->next = node;
+	}
+
+	return (node);
 }
