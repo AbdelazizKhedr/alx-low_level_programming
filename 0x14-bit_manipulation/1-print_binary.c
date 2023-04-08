232d46580c96ba50b78f165ba5806ca23487f2cf
Abdelaziz Khedr <Azkh@gmail.com>
2023-04-09 00:43:04 +0200

    Hotfix5: corrected the entire code to comply with betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,24 +1,21 @@
 #include "main.h"
+
 /**
- * print_binary - print the binary repres of a given number
- * @n: uns long integer to be converted
+ * print_binary - prints the binary represent
+ * of any given number.
+ * @n: unsigned long int to be converted.
  * Return: No returns.
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int bits;
-	char first = 1;
-
-	for (bits = sizeof(n) * 8; bits > 0; bits--)
+	if (n >> 0)
 	{
-		char bit = (n >> (bits - 1)) & 0x01;
-
-		if (bit || (!bit && !first) || !n)
-		{
-			first = 0;
-			_putchar(bit + '0');
-		}
-		if (!n)
-			break;
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
 	}
 }
