4a4f96b5803d96e3928a4f21911f0be4a1eed2d1
Abdelaziz Khedr <Azkh@gmail.com>
2023-04-09 00:52:29 +0200

    Hotfix 8

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary representation
- * of a number.
- * @n: unsigned long int.
- *
- * Return: no return.
+ * print_binary - print number in base2
+ * @n: the number to print
  */
 void print_binary(unsigned long int n)
 {
-	if (n >> 0)
+	unsigned int bits;
+	char first = 1;
+
+	for (bits = sizeof(n) * 8; bits > 0; bits--)
 	{
-		if (n >> 1)
-			print_binary(n >> 1);
-		_putchar((n & 1) + '0');
-	}
-	else
-	{
-		_putchar('0');
+		char bit = (n >> (bits - 1)) & 0x01;
+
+		if (bit || (!bit && !first) || !n)
+		{
+			first = 0;
+			_putchar(bit + '0');
+		}
+		if (!n)
+			break;
 	}
 }
