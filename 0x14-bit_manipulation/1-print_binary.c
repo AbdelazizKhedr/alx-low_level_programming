6588c3b9bb3c667cb3efec3f81adbeea994502fd
Abdelaziz Khedr <Azkh@gmail.com>
2023-04-09 00:50:43 +0200

    Hotfix7

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_binary - prints the bin represent
- * of any given number.
- * @n: unsigned long int to be
- * converted
- * Return: No returns.
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
  */
 void print_binary(unsigned long int n)
 {
